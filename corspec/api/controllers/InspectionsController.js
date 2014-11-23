0ec9814a572f93ca7488acf776bcf1050b326850
cwhite911 <smortopahri@gmail.com>
2014-11-22 21:39:31 -0500

    Added sails framework

---
+++ corspec/api/controllers/InspectionsController.js
@@ -0,0 +1,11 @@
+/**
+ * InspectionsController
+ *
+ * @description :: Server-side logic for managing inspections
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ corspec/api/controllers/InspectorController.js
@@ -0,0 +1,11 @@
+/**
+ * InspectorController
+ *
+ * @description :: Server-side logic for managing inspectors
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
